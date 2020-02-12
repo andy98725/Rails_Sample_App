2bddc4880f8d09c6f34c5447c0ebd1a84f21d19a
= <=>
2020-02-12 00:30:26 +0000

    Adjust page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Get full title of a page
+  def full_title(page_title = '')
+    base_title = "Sample Page"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
