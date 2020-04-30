9112c259874c75218a8932a32dd3522aa60456af
= <=>
2020-04-30 20:31:30 +0000

    Finished User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+      flash[:success] = "Welcome to Sample Page, " + @user.name + "!!"
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Render success
+      flash[:success] = "Welcome to Sample Page, " + @user.name + "!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :uName, :email, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "Rejects Bad Signup" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user: {
+                  name: "", uName:"",
+                  email: "user@invalid",
+                  password: "foo",
+                  password_confirmation: "bar"
+
+                  }}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "Accepts Good Signup" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {
+                  name: "Bobby Joe", uName:"Sample123",
+                  email: "example@railstutorial.com",
+                  password: "fullPassword",
+                  password_confirmation: "fullPassword"
+
+                  }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
